bbe029ca6b1b7c1c86535461237cb2a6b3543418
vrthedev <vishnurao@outlook.com>
2022-09-13 18:19:31 +0900

    firebase change

--- controllers/FirebaseController.js
+++ controllers/FirebaseController.js
@@ -1,7 +1,7 @@
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./legacy-app-53b6d-firebase-adminsdk-yqjd8-21322fa6cf.json");
+var serviceAccount = require("./legacy-app-53b6d-firebase-adminsdk.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
